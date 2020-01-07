958371295ee478208fdc23b702b47853d1f5b22e
techievishal <monstervishal@yahoo.com>
2020-01-07 12:01:29 +0530

    scheduler updated

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -4,6 +4,8 @@ namespace App\Console;
 
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
+use App\SchedulerReminder;
+use DB;
 
 class Kernel extends ConsoleKernel
 {
@@ -14,6 +16,7 @@ class Kernel extends ConsoleKernel
      */
     protected $commands = [
         Commands\SendCustomerNotifications::class,
+		Commands\WordOfTheDay::class,
     ];
 
     /**
@@ -24,25 +27,93 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
+		$reminders = SchedulerReminder::where([
+            'inactivate' => 0
+        ])->get()->toArray();
+		
+
+		foreach($reminders as $reminder){
+			$id = $reminder['id'];
+			$time = $reminder['time_scheduled'];
+			$days = explode(',',$reminder['day_scheduled']);
+			 foreach($days as $day){
+				 switch($day){
+					 case "Sunday":
+					    $schedule->command('word:day',['user' => "Sunday", '--id' => [5, 13]])
+								 ->sundays()->at($time);
+					 break;					 
+					 case "Monday":
+					 	$schedule->command('word:day',['user' => "Monday", '--id' => [5, 13]])
+								 ->mondays()->at($time);
+					 break;
+					 case "Tuesday":
+					 	$schedule->command('word:day',['user' => "Tuesday", '--id' => [5, 13]])
+								 ->tuesdays()->at($time);
+					 break;
+					 case "Wednesday":
+					 	$schedule->command('word:day',['user' => "Wednesday", '--id' => [5, 13]])
+								 ->wednesdays()->at($time);
+					 break;
+					 case "Thursday":
+					 	$schedule->command('word:day',['user' => "Thursday", '--id' => [5, 13]])
+								 ->thursdays()->at($time);
+					 break;
+					 case "Friday":					 
+					 	$schedule->command('word:day',['user' => "Friday", '--id' => [5, 13]])
+								 ->fridays()->at('11:56:01');
+					 break;
+					 case "Saturday":
+					 
+					 	$schedule->command('word:day',['user' => "Saturday", '--id' => [5, 13]])
+								 ->saturdays();
+					 break;					 
+				 }
+				 
+			 }
+			 $this->updateCounter($id);
+		} 
+		date_default_timezone_set('Asia/Kolkata');
+
+		echo date_default_timezone_get();
+
+//$schedule->command('word:day')->cron('18 0 * * MON');
+
+
+		$schedule->command('word:day')            
+			->between('00:29', '00:31');
+			
+		/*$schedule->command('word:day')
+            ->saturdays()
+			->at('18:18:02');*/
+		/*$schedule->command('word:day')
+            ->daily();*/
+			
+			
+	//$schedule->command('word:day',['user' => 'TestUser'])->everyMinute();
         // Run once per week on Monday at 1 PM....
-        $schedule->command('echo "Happy New Year!" ')->everyMinute(); //<---- ADD HERE        
+        /*$schedule->command('echo "Happy New Year!" ')->everyMinute(); //<---- ADD HERE        
 
 
         // Here we can first connect our DB and check settings saved by every user
         // and the 13:00, monday all this stuffs we can replace dynamically
-        $schedule->command('notify:schedule')
+        $schedule->command('word:day')
         ->weekly()->mondays()->at('17:00');
 
-        $schedule->command('notify:schedule')
+        $schedule->command('word:day')
         ->weekly()->mondays()->at('17:02');
-
+*/
         // Run hourly from 8 AM to 5 PM on weekdays...
-        /*$schedule->command('notify:schedule')
+        /*$schedule->command('word:day')
                 ->weekdays()
                 ->hourly()
                 ->timezone('America/Chicago')
                 ->between('8:00', '17:00'); */
     }
+	
+	public function updateCounter($id){
+		
+		
+	}
 
     /**
      * Register the commands for the application.
